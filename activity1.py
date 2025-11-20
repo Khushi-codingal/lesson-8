a2b9254547c2b06a844e59f3554aa7cccd2f26df
Khushi-codingal <rashmiabc@gmail.com>
2025-11-20 17:20:19 +0530

    first commit

---
+++ activity1.py
@@ -0,0 +1,15 @@
+a = 4
+b = 5
+c = 8
+d = 2
+e = 0
+e = (a+b)*c/d;
+print("value of(a+b)*c/d" , e)
+
+
+name = "Alex"
+age = 0
+if name == "Alex" or name=="John" and age>=2:
+    print("hello! welcom")
+else:
+    print("goodbye")
